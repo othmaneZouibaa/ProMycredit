d334d834268548e6e62f380b444d07f92162bf24
othmaneZouibaa <othmanabcd6@gmail.com>
2025-11-25 19:55:37 +0100

    new projet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './myCridit/App';
+import { BrowserRouter } from 'react-router-dom';
 // import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+ </BrowserRouter>
 );
 
